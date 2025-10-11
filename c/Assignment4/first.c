3c2a078631eaa9bb34dcb2e5100629cea68ac214
Ruchi Agrawal <ruchiagrawal9186@gmail.com>
2025-10-11 21:21:24 +0530

    asssignment 4 complated with arithmatic operator

---
+++ c/Assignment4/first.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    char ch='A';
+    float f = 24.6;
+    printf("size of charcter = %d", sizeof(ch));
+    printf("size of float/ real constatnt = %d", sizeof(f));
+}
---
+++ c/Assignment4/second.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    char ch= "A";
+    ch++;
+    printf("increment of charchter = %c", ch);
+}
