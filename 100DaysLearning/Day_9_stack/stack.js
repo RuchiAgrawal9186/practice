c96799e85a681e84888a95abf7308c8b0e14413d
Ruchi Agrawal <ruchiagrawal9186@gmail.com>
2025-10-27 18:34:54 +0530

    day 9 stack data structure with js

---
+++ 100DaysLearning/Day_9_stack/stack.js
@@ -0,0 +1,43 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(ele) {
+    this.items.push(ele);
+  }
+
+    pop() {
+      if (this.isEmpty()) {
+        return null;
+      }
+    let removeEle = this.items.pop();
+    return removeEle;
+  }
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.items[this.items.length - 1];
+  }
+  isEmpty() {
+    return this.items.length === 0;
+  }
+  getSize() {
+    return this.items.length;
+  }
+}
+
+const newStack = new Stack()
+newStack.push(10)
+newStack.push(20)
+console.log("top element = ", newStack.peek());
+newStack.push(30)
+console.log("removed element = ", newStack.pop())
+newStack.push(40)
+console.log("top element = ", newStack.peek())
+newStack.push(50);
+console.log("removed element = ", newStack.pop());
+console.log("removed element = ", newStack.pop());
+newStack.push(90);
+console.log("top element = ", newStack.peek());
